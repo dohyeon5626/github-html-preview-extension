21d4041be1a95188bdf6a96e3036efda8d758283
dohyeon5626 <kodohyeon71@gmail.com>
2022-12-17 19:17:10 +0900

    Change base settings
    - change webpack.config.js
    - change tsconfig.json
    - add base template
    
    Close #1

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
    mode: "production",
    entry: {
-      background: path.resolve(__dirname, "..", "src", "background.ts"),
+      background: ["./src/background/background.ts"],
+      popup: ["./src/popup/popup.ts"],
+      content: ["./src/content/content.ts"]
    },
    output: {
       path: path.join(__dirname, "../dist"),
-      filename: "[name].js",
-   },
-   resolve: {
-      extensions: [".ts", ".js"],
+      filename: "[name].js"
    },
    module: {
       rules: [
          {
-            test: /\.tsx?$/,
+            test: /\.ts$/,
             loader: "ts-loader",
-            exclude: /node_modules/,
-         },
-      ],
+            exclude: /node_modules/
+         }
+      ]
    },
    plugins: [
       new CopyPlugin({
          patterns: [{from: ".", to: ".", context: "public"}]
-      }),
-   ],
+      })
+   ]
 };
